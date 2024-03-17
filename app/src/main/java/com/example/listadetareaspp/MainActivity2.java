9395bac65fac2bacfefa86c6f49c74658be5356c
Canul <canuladriana24@gmail.com>
2024-03-17 00:29:40 -0600

    Avance de la interfaz

--- app/src/main/java/com/example/listadetareaspp/MainActivity2.java
+++ app/src/main/java/com/example/listadetareaspp/MainActivity2.java
@@ -1,6 +1,10 @@
 package com.example.listadetareaspp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button agregar;
+    Button eliminar;
+    EditText tarea_nueva;
+    TextView lista_tareas;
+    String lista;
+    String tn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        agregar=(Button) findViewById(R.id.btnAdd);
+        tarea_nueva=(EditText) findViewById(R.id.etTarea);
+        lista_tareas=(TextView) findViewById(R.id.txtLista);
+
+        agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lista=lista_tareas.getText().toString();
+                tn=tarea_nueva.getText().toString();
+                lista=lista + "\n" +tn;
+                lista_tareas.setText(lista);
+            }
         });
     }
 }
